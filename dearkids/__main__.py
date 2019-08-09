c51d7e8c42cefc54d6ed823961487e90eb66390e
David Larlet <david@larlet.fr>
2019-08-09 08:26:53 -0400

    🇺🇳 Deal with i18n to have a French version

--- dearkids/__main__.py
+++ dearkids/__main__.py
@@ -1,9 +1,10 @@
 import asyncio
 from pathlib import Path
 
+import jinja2
 import uvloop
 from jinja2 import Environment, PackageLoader, select_autoescape
-from roll import Response
+from roll import Request, Response
 from roll import Roll as BaseRoll
 from roll.extensions import logger, simple_server, static, traceback
 
@@ -16,13 +17,40 @@ env = Environment(
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
+def i18n(app):
+    @app.listen("request")
+    async def guess_language(request, response):
+        accept_language = request.headers.get("ACCEPT-LANGUAGE")
+        # Very naive, yet very efficient?
+        request.language = accept_language[:2] if accept_language else "en"
+
+
+class LanguageAwareRequest(Request):
+    __slots__ = (
+        "app",
+        "url",
+        "path",
+        "query_string",
+        "_query",
+        "method",
+        "body",
+        "headers",
+        "route",
+        "_cookies",
+        "_form",
+        "_files",
+        "language",  # Added.
+    )
+
+
 class HTMLResponse(Response):
-    def html(self, template_name, *args, **kwargs):
+    def html(self, template, *args, **kwargs):
         self.headers["Content-Type"] = "text/html; charset=utf-8"
-        self.body = env.get_template(template_name).render(*args, **kwargs)
+        self.body = template.render(*args, **kwargs)
 
 
 class Roll(BaseRoll):
+    Request = LanguageAwareRequest
     Response = HTMLResponse
 
 
@@ -30,11 +58,21 @@ app = Roll()
 logger(app)
 traceback(app)
 static(app, root=HERE.parent / "static")
+i18n(app)
+
+
+def language_aware_template(template_name, language_code):
+    try:
+        return env.get_template(f"{language_code}.{template_name}")
+    except jinja2.exceptions.TemplateNotFound:
+        return env.get_template(template_name)
 
 
 @app.route("/")
 async def home(request, response):
-    response.html("home.html")
+    template_name = "home.html"
+    template = language_aware_template(template_name, request.language)
+    response.html(template)
 
 
 if __name__ == "__main__":
--- tests/test_app.py
+++ tests/test_app.py
@@ -19,6 +19,13 @@ async def test_home(client, app):
     assert b"<h1>Dear kids,</h1>" in resp.body
 
 
+async def test_home_fr(client, app):
+    resp = await client.get("/", headers={"Accept-language": "fr-CA"})
+    assert resp.status == HTTPStatus.OK
+    assert resp.headers["Content-Type"] == "text/html; charset=utf-8"
+    assert b"<h1>Les enfants,</h1>" in resp.body
+
+
 async def test_stylesheet(client, app):
     resp = await client.get("/static/styles.css")
     assert resp.status == HTTPStatus.OK
